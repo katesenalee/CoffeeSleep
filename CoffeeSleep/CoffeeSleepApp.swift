a08f215e70e7efa6dfab63b618d795ee9e33fbfe
sena <43688294+katesenalee@users.noreply.github.com>
2021-10-25 18:04:47 +0800

    Initial Commit

---
+++ CoffeeSleep/CoffeeSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeSleepApp.swift
+//  CoffeeSleep
+//
+//  Created by Sena Lee on 10/25/21.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
